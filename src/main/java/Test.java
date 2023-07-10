8dd826683b896720c29c341ce1daec0c5e93107b
Prashant Singh Basnet <prashantsingh.basnet@cotiviti.com>
2023-07-10 18:14:19 +0545

    hello world prg

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    void PrintTest() {
+        System.out.println("Hello World!");
+    }
+
+   public static void main (String[] args){
+      Test ts = new Test();
+       ts.PrintTest();
+   }
+
+
+}
